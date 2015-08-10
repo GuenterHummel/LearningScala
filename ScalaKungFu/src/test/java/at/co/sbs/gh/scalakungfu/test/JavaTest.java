f26fe1ec26113e8cfd3a5924a1f656c7a6770795
Günter Hummel <guenter.hummel@sbs.co.at>
2015-08-10 16:02:13 +0200

    ScalaKungFu
    Learning Scala and Functional Programming

---
+++ ScalaKungFu/src/test/java/at/co/sbs/gh/scalakungfu/test/JavaTest.java
@@ -0,0 +1,20 @@
+package at.co.sbs.gh.scalakungfu.test;
+
+import at.co.sbs.gh.scalakungfu.EchoArg;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: GH
+ * Date: 07.11.13
+ * Time: 00:39
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaTest {
+
+    public static void main (String[] args) {
+
+        System.out.println("Calling Scala Object Main Method\n");
+
+        EchoArg.main(args);
+    }
+}
