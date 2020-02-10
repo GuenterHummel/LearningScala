4447aea47006452febfc4093f2e5aa430c8707ec
Günter Hummel <guenter.hummel@sbs.co.at>
2020-02-10 16:33:25 +0100

    rework of project

---
+++ playground/src/main/scala/at/co/sbs/gh/learning/scala/helloarg.scala
@@ -0,0 +1,5 @@
+object Helloarg {
+  def main( args:Array[String]) {
+    println("Hello, " + args(0) + "!")
+  }
+}
---
+++ playground/src/main/scala/at/co/sbs/gh/learning/scala/printargs.scala
@@ -0,0 +1,9 @@
+object Printargs {
+	def main(args: Array[String]) {
+		var i = 0
+		while (i < args.length) {
+			println(args(i))
+			i += 1
+		}
+	}
+}
